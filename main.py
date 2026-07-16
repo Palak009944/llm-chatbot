8d5930c31653040a5c20ff8f564985a27c12b599
Palak009944 <palaksobti2406@gmail.com>
2026-07-16 12:23:16 +0530

    Completed the LLM call

--- main.py
+++ main.py
@@ -6,9 +6,25 @@ load_dotenv()
 api_key=os.getenv("Gemini_API_Key")
 
 client=genai.Client(api_key=api_key)
-response=client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents="Hello! How are you?"
-)
 
-print(response.text)
+while True:
+    user_prompt=input("You: ")
+
+    if user_prompt.lower()=="exit":
+        print("Goodbye!")
+        break
+    else:
+        try:
+            response=client.models.generate_content(
+                model="gemini-3.5-flash",
+                contents=user_prompt)
+            print("AI:",response.text)
+        except Exception as e:
+            print("Error: ",e)
+
+
+
+
+
+    
+
