5d61c5890d78fe63d91fae3d4fb76ced4a3e38b2
Palak009944 <palaksobti2406@gmail.com>
2026-07-16 12:01:49 +0530

    Client created

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ import os
 load_dotenv()
 api_key=os.getenv("Gemini_API_Key")
 
+client=genai.Client(api_key=api_key)
+response=client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Hello! How are you?"
+)
+
+print(response.text)
