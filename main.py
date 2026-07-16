1474d990c0dac0ddbeb4faa7d47800a2abae99b0
Palak009944 <palaksobti2406@gmail.com>
2026-07-16 13:35:03 +0530

    Added comments

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from google import genai
+from google import genai #this line imports google genai SDK into your program
+# genai module provides classes and methods like Client,models,generate_content
+
 from dotenv import load_dotenv
-import os
+#dotenv is module provided by python-dotenv library
+import os #It provides functions for interacting with the operating system.
 
-load_dotenv()
-api_key=os.getenv("Gemini_API_Key")
+load_dotenv() #Load all variables from the .env file into the program's environment.
+#load_dotenv is a function defined by dotenv module
+#It reads the .env file and loads its contents into the program's environment.
 
-client=genai.Client(api_key=api_key)
+api_key=os.getenv("Gemini_API_Key") #It retrieves the value of an environment variable by its name.
+
+client=genai.Client(api_key=api_key) #Client is a class already written by Google inside the GenAI SDK. 
 
 while True:
     user_prompt=input("You: ")
@@ -16,7 +22,7 @@ while True:
     else:
         try:
             response=client.models.generate_content(
-                model="gemini-3.5-flash",
+                model="gemini-3.5-flash",  #"Flash" is Google's family of fast and efficient models.
                 contents=user_prompt)
             print("AI:",response.text)
         except Exception as e:
